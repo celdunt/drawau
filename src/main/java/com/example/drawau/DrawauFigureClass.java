ca86ad3be1d0ef92462d39a6e30f2d363bdcd183
celdunt <107755206+celdunt@users.noreply.github.com>
2022-07-17 17:42:16 +0300

    initial commit..

---
+++ src/main/java/com/example/drawau/DrawauFigureClass.java
@@ -0,0 +1,72 @@
+package com.example.drawau;
+
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrawauFigureClass implements DrawauIFigure {
+
+    public class FieldProperty {
+        public FieldProperty(String vname, String vtype) {
+            name = vname;
+            type = vtype;
+        }
+
+        private String name;
+        private String type;
+
+        public void setName(String value) {name = value;}
+        public String getName() {return name;}
+
+        public void setType(String value) {type = value;}
+        public String getType() {return type;}
+    }
+
+
+    public static final String TYPE_ABSTRACT = "abstract class";
+    public static final String TYPE_SIMPLE = "class";
+    public static final String TYPE_INTERFACE = "interface";
+
+
+    private String type;
+    private String name;
+    private List<FieldProperty> fields = new ArrayList<>();
+    private List<FieldProperty> methods = new ArrayList<>();
+
+    private VBox container = new VBox();
+
+
+    public void setType(String value) { type = value; }
+    public String getType() { return type; }
+
+    public void setName(String value) { name = value; }
+    public String getName() { return name; }
+
+    public void setFields(FieldProperty... values) {
+        fields.clear();
+        fields.addAll(List.of(values));
+    }
+    public List<FieldProperty> getFields() { return fields; }
+
+    public void setMethods(FieldProperty... values) {
+        methods.clear();
+        methods.addAll(List.of(values));
+    }
+    public List<FieldProperty> getMethods() { return methods; }
+
+    public void setSize(double hvalue, double vvalue) { container.setPrefSize(hvalue, vvalue); }
+    public double getWidth() { return container.getWidth(); }
+    public double getHeight() { return container.getHeight(); }
+
+
+    @Override
+    public void locate() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/main/java/com/example/drawau/DrawauIFigure.java
@@ -0,0 +1,6 @@
+package com.example.drawau;
+
+public interface DrawauIFigure {
+    void locate();
+    void draw();
+}
